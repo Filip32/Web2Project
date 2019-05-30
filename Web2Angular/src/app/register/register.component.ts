a3e49c44e170e70e08504bd923b799c1ac5fb1ac
krstic13 <krstic.andjela13@gmail.com>
2019-05-30 22:10:15 +0200

    validation

--- Web2Angular/src/app/register/register.component.ts
+++ Web2Angular/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { ConfirmPasswordValidator } from './confirm-password.validator';
 
 @Component({
   selector: 'app-register',
@@ -8,24 +9,46 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  registerForm:FormGroup;
+  submitted:boolean = false;
+  get f() { return this.registerForm.controls; }
   constructor(private fb: FormBuilder) { }
 
-  registerForm = this.fb.group({
-    Email: ['', Validators.required],
-    Password: [''],
-    PasswordR:[''],
-    Name: [''],
-    Lastname: [''],
-    Address: [''],
-    Birthday:[''],
-    UserType:['']
-  });
-
   ngOnInit() {
-  }
+    this.registerForm = this.fb.group({
+      Email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword:['', Validators.required],
+      Name: [''],
+      Lastname: [''],
+      Address: [''],
+      Birthday:[''],
+      UserType:['']
+    },{
+      validator: MustMatch('password', 'confirmPassword')
+    })};
 
   onSubmit() {
     console.warn(this.registerForm.value);
+    this.submitted = true;
+  }
+}
+
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          // return if another validator has already found an error on the matchingControl
+          return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
   }
 }
