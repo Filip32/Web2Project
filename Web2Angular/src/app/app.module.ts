cc71f45814639713f4d13127457ce53a03b85634
krstic13 <krstic.andjela13@gmail.com>
2019-05-30 17:13:42 +0200

    Group for login finished

--- Web2Angular/src/app/app.module.ts
+++ Web2Angular/src/app/app.module.ts
@@ -7,12 +7,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
-=======
 import { TimetableComponent } from './timetable/timetable.component';
 import { PricelistComponent } from './pricelist/pricelist.component';
->>>>>>> 55ab55d4561ccf6e43b03287d8feac4fd94c4ee9
 
 @NgModule({
   declarations: [
